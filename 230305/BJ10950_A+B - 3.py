5f4913277e0c5bb5923cd9e2b412c16a3a8fc80d
김승규 사원 <seungkyu.kim@dkbmc.com>
2023-02-21 19:11:09 +0900

    done

--- 230305/BJ10950_A+B - 3.py
+++ 230305/BJ10950_A+B - 3.py
@@ -2,3 +2,9 @@
 # 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
 # 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 # 각 테스트 케이스마다 A+B를 출력한다.
+import sys
+
+t = int(sys.stdin.readline())
+for i in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    print(a+b, end=' ')
