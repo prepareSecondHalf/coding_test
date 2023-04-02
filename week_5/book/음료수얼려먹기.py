6fbbff90044e932f33cc417c3eddbcd4f96bc0d7
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2023-04-02 09:42:48 +0900

    week 5

--- week_5/book/음료수얼려먹기.py
+++ week_5/book/음료수얼려먹기.py
@@ -11,12 +11,15 @@ for i in range(n):
 def dfs(x, y):
 	if x < 0 or x >= n or y < 0 or y >= m:
 		return False
+	
 	if graph[x][y] == 0:
 		graph[x][y] = 1
+
 		dfs(x + 1, y)
 		dfs(x - 1, y)
 		dfs(x, y + 1)
 		dfs(x, y - 1)
+
 		return True
 	return False
 
